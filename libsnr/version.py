fbdca0bc55e5d088eb6b3857d996104201749b91
Kamyar <139503563+GlobularOne@users.noreply.github.com>
2024-02-28 14:24:17 +0330

    libsnr/version.py: Changed version for new release

--- libsnr/version.py
+++ libsnr/version.py
@@ -6,10 +6,10 @@ Module containing snr version info
 MAJOR = "0"
 
 ## libsnr version minor (Y.X.Y)
-MINOR = "0"
+MINOR = "1"
 
 ## libsnr version patch (Y.Y.X)
-PATCH = "1"
+PATCH = "0"
 
 ## libsnr homepage
 HOMEPAGE = "https://github.com/GlobularOne/snr-meta"
